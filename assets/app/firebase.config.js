73a34f8b55b524c7445eb32dcd33b682c6c95c8d
SST-maker <nacerito83@gmail.com>
2026-07-06 18:50:27 +0200

    Update firebase.config.js

--- assets/app/firebase.config.js
+++ assets/app/firebase.config.js
@@ -1,14 +1,15 @@
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: "AIzaSyB2eBAUe21kCFj8MIUUZx3aPPq0hScWiYw",
+  authDomain: "ncr-academy.firebaseapp.com",
+  projectId: "ncr-academy",
+  storageBucket: "ncr-academy.firebasestorage.app",
+  messagingSenderId: "772654866749",
+  appId: "1:772654866749:web:ca609b77c6877e725a2554",
+  measurementId: "G-02E4CC3TDB"
 };
 
 export const firebaseRuntime = {
-  enabled:false,
+  enabled: true,
   allowDemoFallback: true,
   sdkVersion: '10.12.5',
   defaultCourses: [
