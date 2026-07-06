5d954f613f4888494157a15ff5173d47ba9c6971
SST-maker <nacerito83@gmail.com>
2026-07-06 20:42:03 +0200

    Add files via upload

---
+++ assets/app/supabase.config.js
@@ -0,0 +1,16 @@
+export const supabaseConfig = {
+  url: 'https://hoxrjuyhrnyrkmanlyoc.supabase.co',
+  anonKey: 'ssb_publishable_C9GsXGK0sHCVsD2BCbFI8Q_u9Ky9pHK'
+};
+
+export const supabaseRuntime = {
+  enabled: true,
+  allowDemoFallback: true,
+  defaultCourses: [
+    'communication-digitale',
+    'outils-bureautiques',
+    'litiges-clients',
+    'community-manager',
+    'acteur-sst'
+  ]
+};
