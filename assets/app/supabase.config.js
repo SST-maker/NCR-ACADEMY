fc774512ff9c14c3c5e25cbb2845dc0baed2988a
SST-maker <nacerito83@gmail.com>
2026-07-07 19:19:13 +0200

    Update supabase.config.js

--- assets/app/supabase.config.js
+++ assets/app/supabase.config.js
@@ -1,8 +1,8 @@
 export const supabaseConfig = {
-  url: '',
-  anonKey: '',
-  publishableKey: '',
-  apiKey: ''
+  url: 'https://snatoenrtiybjmmitfvn.supabase.co',
+  anonKey: 'sb_publishable_di4i2kdEP9Lnuf-vlQU01g_NFGAPsvG',
+  publishableKey: 'sb_publishable_di4i2kdEP9Lnuf-vlQU01g_NFGAPsvG',
+  apiKey: 'sb_publishable_di4i2kdEP9Lnuf-vlQU01g_NFGAPsvG'
 };
 
 export const supabaseRuntime = {
